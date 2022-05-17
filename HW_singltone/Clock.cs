06a98da84d212510dbd844faf18c82c2897c6708
NaorA <NaorA@DESKTOP-1S1L5GV>
2022-05-17 23:02:14 +0300

    Add project files.

---
+++ HW_singltone/Clock.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_singltone
+{
+    internal class Clock
+    {
+
+
+
+        private static Clock instance;
+        private static readonly object Key = new object();
+
+
+        private Clock()
+        {
+
+        }
+        public static Clock GetTime()
+        {
+            if (instance == null)
+            {
+                lock (Key)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Clock();
+                    }
+
+                }
+
+
+
+            }
+            return instance;
+        }
+
+        public override string ToString()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+
+}
---
+++ HW_singltone/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW_singltone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
